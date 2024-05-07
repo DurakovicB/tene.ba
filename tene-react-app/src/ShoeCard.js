bcea19073738a138dcefbe64c8e28bca61190442
Bilal Durakovic <bilal.durakovic@stu.ibu.edu.ba>
2024-05-07 15:44:03 +0200

    updated UI

--- tene-react-app/src/ShoeCard.js
+++ tene-react-app/src/ShoeCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './ShoeCard.css';
 
 const ShoeCard = ({ shoe }) => {
   return (
     <div className="card">
-      <img src={shoe.image_url} className="card-img-top" alt={shoe.title} />
+      <div className="card-img-container">
+        <img src={shoe.image_url} className="card-img" alt={shoe.title} />
+      </div>
       <div className="card-body">
         <h5 className="card-title">{shoe.name}</h5>
         <p className="card-text">Brand: {shoe.brand}</p>
         <p className="card-text">Price: {shoe.price} KM</p>
-        <a href={shoe.link} className="btn btn-primary">View Details</a>
+        <a href={shoe.link}  target="_blank" rel="noopener noreferrer">View Details</a>
       </div>
     </div>
   );
